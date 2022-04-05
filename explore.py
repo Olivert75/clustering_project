fcb88175fda149e15362f632a269b7d22df9bf9d
Oliver Ton <oliver.ton75@gmail.com>
2022-04-05 17:23:39 -0500

    adding some function for clustering

--- explore.py
+++ explore.py
@@ -1,6 +1,63 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 
+def explore_univariate(df, variable):
+    '''
+    functions to create clusters and scatter-plot:
+    explore_univariate will take in a dataframe, and one feature or variable. It graphs a box plot and a distribution 
+    of the single variable.
+    '''
+    #set figure size, font for axis ticks, and turns off gridlines.
+    plt.figure(figsize=(30,10))
+    sns.set(font_scale = 2)
+    sns.set_style("whitegrid", {'axes.grid' : False})
+    
+    # boxplot
+    plt.subplot(1, 2, 1)
+    sns.boxplot(x=variable, data=df)
+    plt.xlabel('')
+    plt.title('Box Plot', fontsize=30)
+    
+    # distribution
+    plt.subplot(1, 2, 2)
+    sns.histplot(data=df, x=variable, element='step', kde=True, color='blue')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.title('Distribution', fontsize=30)
+    
+    #title
+    plt.suptitle(f'{variable}', fontsize = 45)
+    plt.tight_layout()
+    plt.show()
+
+def create_cluster(df, X, k):
+    
+    """ Takes in df, X (dataframe with variables you want to cluster on) and k
+    # It scales the X, calcuates the clusters and return train (with clusters), the Scaled dataframe,
+    #the scaler and kmeans object and unscaled centroids as a dataframe"""
+    
+    scaler = StandardScaler(copy=True).fit(X)
+    X_scaled = pd.DataFrame(scaler.transform(X), columns=X.columns.values).set_index([X.index.values])
+    kmeans = KMeans(n_clusters = k, random_state = 42)
+    kmeans.fit(X_scaled)
+    kmeans.predict(X_scaled)
+    df['cluster'] = kmeans.predict(X_scaled)
+    df['cluster'] = 'cluster_' + df.cluster.astype(str)
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    return df, X_scaled, scaler, kmeans, centroids
+
+def create_scatter_plot(x,y,df,kmeans, X_scaled, scaler):
+    
+    """ Takes in x and y (variable names as strings, along with returned objects from previous
+    function create_cluster and creates a plot"""
+    
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x = x, y = y, data = df, hue = 'cluster')
+    centroids = pd.DataFrame(scaler.inverse_transform(kmeans.cluster_centers_), columns=X_scaled.columns)
+    centroids.plot.scatter(y=y, x= x, ax=plt.gca(), alpha=.30, s=500, c='black')
 
 def plot_variable_pairs(train, cols, hue=None):
     '''
